d85722cc15eef9b529d85326f0cd5b8bc7218ce7
raihanJa <raihanja@gmail.com>
2023-09-24 17:13:11 +0200

    added button section

---
+++ client/src/components/LandingPage/LandingButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="custom-button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "LandingButton"
+}
+</script>
+
+<style scoped>
+  .custom-button{
+    padding: 10px 20px;
+    color: white;
+    border: solid 2px #DE6520;
+    border-radius: 12px;
+    cursor: pointer;
+    margin-bottom: 1%;
+    font-size: 24px;
+    width: 12%;
+    background-color: #DE6520;
+    transition: background-color 0.5s, color 0.5s, border 0.5s;
+  }
+
+  .custom-button:hover{
+    animation: none;
+    background-color: white;
+    color: black;
+    border: solid 2px #DE6520;
+  }
+
+</style>
--- client/src/components/LandingPage/LandingComponent.vue
+++ client/src/components/LandingPage/LandingComponent.vue
@@ -1,52 +1,74 @@
 <template>
   <div class="landing">
-    <div class="centered-text">
-      <h1>Give yourself a break</h1><br>
-      <p>Online Therapy</p><br>
-      <p>Anywhere.Anytime. Your fee</p>
-    </div>
+      <div class="centered-text">
+        <h1>Give yourself a break</h1><br>
+        <p>Online Therapy</p><br>
+        <p>Anywhere.Anytime. Your fee</p>
+      </div>
+
+      <div class="button-section">
+        <LandingButton>I'm a patient</LandingButton>
+        <br>
+        <LandingButton>I'm a therapist</LandingButton>
+      </div>
+
   </div>
 
 </template>
 
 <script>
+import LandingButton from "@/components/LandingPage/LandingButton";
+
 export default {
-  name: "LandingComponent"
+  name: "LandingComponent",
+  components: {
+    LandingButton
+  }
 }
 </script>
 
 <style scoped>
-  .landing{
-    background-image: url("../../assets/img/landing-background.jpg");
-    background-size: cover;
-    background-position: center;
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+.landing {
+  background-image: url("../../assets/img/landing-background.jpg");
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100vh;
+}
 
-  .centered-text h1, p {
-    margin: 0;
-    padding: 0;
-    color: white;
-  }
+.centered-text {
+  text-align: center;
+  background: rgba(0, 0, 0, 0.5);
+  width: 75%;
+  padding-top: 3%;
+  padding-bottom: 3%;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  top: 30%;
+}
 
-  .centered-text h1{
-    font-size: 120px;
-    font-weight: normal;
-  }
+.centered-text h1, p {
+  margin: 0;
+  padding: 0;
+  color: white;
+}
 
-  .centered-text p{
-    font-size: 48px;
-  }
+.centered-text h1 {
+  font-size: 120px;
+  font-weight: normal;
+}
 
-  .centered-text{
-    text-align: center;
-    background: rgba(0, 0, 0, 0.5);
-    width: 75%;
-    padding-top: 3%;
-    padding-bottom: 3%;
-  }
+.centered-text p {
+  font-size: 48px;
+}
+
+.button-section {
+  text-align: center;
+  position: relative;
+  top: 30%;
+  margin-top: 2%;
+  overflow: hidden;
+}
 
 </style>
