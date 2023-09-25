876ca30bcd3f40dc5df2d6602851d21e51bfb01a
raihanJa <raihanja@gmail.com>
2023-09-25 14:03:54 +0200

    fixed issues

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,8 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import HomeComponent from "@/components/HomePage/HomeComponent";
 import LandingComponent from "@/components/LandingPage/LandingComponent";
-<<<<<<< HEAD
 import HomeProfessionals from "@/components/HomeProfessionals";
-=======
 import FindDoctor from "@/components/FindDoctorPage/FindDoctor";
->>>>>>> 618deeea5556502ad297c80435da9e66c7adbe22
 
 const routes = [
     {
@@ -17,14 +14,15 @@ const routes = [
         component: LandingComponent,
     },
     {
-<<<<<<< HEAD
+
         path: "/home-professionals",
         component: HomeProfessionals,
-=======
+    },
+    {
         path: "/find-doctor",
         component: FindDoctor,
         props: true
->>>>>>> 618deeea5556502ad297c80435da9e66c7adbe22
+
     }
 ]
 
