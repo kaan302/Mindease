fcc72e6140f1f0cadc2e2e15a889173d83c768d9
raihanJa <raihanja@gmail.com>
2023-09-25 15:18:47 +0200

    made profiles

--- client/src/components/FindDoctorPage/DoctorDetail.vue
+++ client/src/components/FindDoctorPage/DoctorDetail.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="doctor-detail">
-    <!-- Display doctor information -->
-    <h2>{{ doctor.doctorFirstName }} {{ doctor.doctorLastName }}</h2>
-    <p>Profession: {{ doctor.doctorProfession }}</p>
-    <p>Language: {{ doctor.doctorLanguage }}</p>
-    <p>Rating: {{ doctor.doctorRating }}</p>
-    <!-- Add more doctor details as needed -->
+    <!-- Circular profile picture -->
+    <div class="profile-picture">
+      <img src="../../assets/img/team-1.jpg" alt="Doctor's Profile" />
+    </div>
+
+    <!-- Doctor information -->
+    <div class="doctor-info">
+      <h2>{{ doctor.doctorFirstName }} {{ doctor.doctorLastName }}</h2>
+      <p>Profession: {{ doctor.doctorProfession }}</p>
+      <p>Language: {{ doctor.doctorLanguage }}</p>
+      <p>Rating: {{ doctor.doctorRating }}</p>
+      <!-- Add more doctor details as needed -->
+    </div>
   </div>
 </template>
 
@@ -19,5 +26,28 @@ export default {
 </script>
 
 <style scoped>
+.doctor-detail {
+  align-items: center;
+  gap: 20px;
+  background: yellow;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  padding: 20px; /* Add padding for spacing */
+}
 
+.profile-picture {
+  width: 100px; /* Adjust the size of the circular picture */
+  height: 100px; /* Adjust the size of the circular picture */
+  border-radius: 50%; /* Create a circular shape */
+  overflow: hidden; /* Hide any overflowing content */
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5); /* Optional: Add a shadow */
+}
+
+.profile-picture img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover; /* Ensure the image covers the circular area */
+}
 </style>
--- client/src/components/FindDoctorPage/FindDoctorChild.vue
+++ client/src/components/FindDoctorPage/FindDoctorChild.vue
@@ -1,8 +1,12 @@
 <template>
 
-  <h1>{{ selectedProfession }}</h1>
-  <div v-for="doctor in doctors" :key="doctor.doctorId">
-    <DoctorDetail :doctor="doctor"/>
+  <div>
+    <h1>{{ selectedProfession }}</h1>
+    <div class="row">
+      <div v-for="doctor in doctors" :key="doctor.doctorId" class="column">
+        <DoctorDetail :doctor="doctor" class="doctor-detail" />
+      </div>
+    </div>
   </div>
 
 </template>
@@ -25,5 +29,24 @@ export default {
 <style scoped>
 h1 {
   text-align: center;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px; /* Adjust the gap between doctors */
+  justify-content: space-between; /* Distribute doctors evenly in rows */
+}
+
+.column {
+  width: calc(25% - 20px); /* 25% width minus the gap between doctors */
+
+}
+
+/* Adjust layout for smaller screens (e.g., mobile devices) */
+@media (max-width: 768px) {
+  .column {
+    width: calc(50% - 20px); /* Two doctors per row on smaller screens */
+  }
 }
 </style>
