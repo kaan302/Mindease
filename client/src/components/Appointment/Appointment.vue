c657294c4f5e48b030b1fa224d477daeb4b5965e
raihanJa <raihanja@gmail.com>
2023-09-26 16:42:54 +0200

    made contact blocks

--- client/src/components/Appointment/Appointment.vue
+++ client/src/components/Appointment/Appointment.vue
@@ -1,37 +1,63 @@
 <template>
   <div class="content">
-    <div class="content-left">
-      <h1>{{ doctor.doctorFirstName + " " + doctor.doctorLastName }}</h1>
-    </div>
-    <div class="content-right">
-      <div class="doctor-info">
-        <p><b class="title">Profession:</b> Cardiologist</p>
-        <p><b class="title">Language:</b> English</p>
-        <p><b class="title">Specialties:</b> Cardiology, Internal Medicine</p>
-        <p><b class="title">Education:</b> MD, University of Medical School</p>
-        <p><b class="title">Locations:</b> New York, Los Angeles</p>
-        <p><b class="title">Patients:</b> 120</p>
-        <p><b class="title">Years of Experience:</b> 10</p>
-        <p><b class="title">Rating:</b> 4.5</p>
-        <p><b class="title">Description:</b></p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          scelerisque quam sed metus condimentum, eu volutpat urna
-          facilisis. Nullam venenatis metus eu ipsum facilisis, ut posuere
-          libero lacinia.
-        </p>
+    <div class="content-doctor-info">
+      <div class="content-left">
+        <h1>{{ doctor.doctorFirstName + " " + doctor.doctorLastName }}</h1>
+      </div>
+      <div class="content-right">
+        <div class="doctor-info">
+          <p><b class="title">Profession:</b> Cardiologist</p>
+          <p><b class="title">Language:</b> English</p>
+          <p><b class="title">Specialties:</b> Cardiology, Internal Medicine</p>
+          <p><b class="title">Education:</b> MD, University of Medical School</p>
+          <p><b class="title">Locations:</b> New York, Los Angeles</p>
+          <p><b class="title">Patients:</b> 120</p>
+          <p><b class="title">Years of Experience:</b> 10</p>
+          <p><b class="title">Rating:</b> 4.5</p>
+          <p><b class="title">Description:</b></p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+            scelerisque quam sed metus condimentum, eu volutpat urna
+            facilisis. Nullam venenatis metus eu ipsum facilisis, ut posuere
+            libero lacinia.
+          </p>
+        </div>
       </div>
     </div>
+
+    <h2>Contact information</h2>
+    <div class="doctor-contact">
+      <DoctorContact
+          class="doctor-contact-item"
+          title="Address"
+          sub-title="123 Street Amsterdam"
+          icon="fas fa-map-marker-alt"/>
+      <DoctorContact
+          class="doctor-contact-item"
+          title="Call"
+          sub-title="+012 345 6789"
+          icon="fas fa-phone"/>
+      <DoctorContact
+          class="doctor-contact-item"
+          title="Mail"
+          sub-title="info@example.com"
+          icon="fas fa-envelope"/>
+    </div>
+
   </div>
 </template>
 
 <script>
+import DoctorContact from "@/components/Appointment/DoctorContact";
+
 export default {
   name: "AppointmentComponent",
   props: {
     doctor: Object,
   },
-  components: {},
+  components: {
+    DoctorContact
+  },
 };
 </script>
 
@@ -40,6 +66,9 @@ export default {
   width: 80%;
   margin-left: auto;
   margin-right: auto;
+}
+
+.content-doctor-info {
   margin-top: 3%;
   display: flex;
   height: 94vh;
@@ -81,6 +110,25 @@ export default {
 .title {
   font-weight: bold;
   margin-right: 10px;
+}
+
+.doctor-contact {
+  display: flex;
+  justify-content: space-between; /* Add space between items */
+  width: 80%;
+  margin: 5% auto;
+}
+
+.doctor-contact-item {
+  flex: 1;
+  margin-right: 5%;
+  margin-left: 5%
+}
+
+h2{
+  text-align: center;
+  margin-top: 5%;
+  font-size: 34px;
 }
 
 </style>
---
+++ client/src/components/Appointment/DoctorContact.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="contact">
+    <i :class="icon"></i>
+    <h2 class="title">{{ title }}</h2>
+    <p class="sub-title">{{ subTitle }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DoctorContact",
+  props: {
+    title: String,
+    subTitle: String,
+    icon: String
+  },
+}
+</script>
+
+<style scoped>
+  .contact{
+    background-color: #E8891D;
+    text-align: center;
+    border-radius: 20px;
+    padding: 2%;
+  }
+
+  .sub-title{
+    font-size: 18px;
+  }
+
+  i{
+    font-size: 20px;
+    background: white;
+    padding: 10%;
+    border-radius: 50%;
+  }
+</style>
