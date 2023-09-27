8af64d9ac9db54114e5dae696868f34bce9b02d7
Kaan Uğur <kaan.ugur@hva.nl>
2023-09-27 15:44:30 +0200

    changed navbar

--- client/src/components/Navigation/Navbar.vue
+++ client/src/components/Navigation/Navbar.vue
@@ -38,19 +38,10 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarCollapse">
       <div class="navbar-nav ms-auto p-4 p-lg-0">
-        <a href="" class="nav-item nav-link active">Home</a>
-        <a href="" class="nav-item nav-link">About</a>
-        <a href="" class="nav-item nav-link">Service</a>
-        <div class="nav-item dropdown">
-          <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
-          <div class="dropdown-menu rounded-0 rounded-bottom m-0">
-            <a href="" class="dropdown-item">Our Doctor</a>
-            <a href="" class="dropdown-item">Appointment</a>
-            <a href="" class="dropdown-item">Testimonial</a>
-            <a href="" class="dropdown-item">404 Page</a>
-          </div>
-        </div>
-        <a href="" class="nav-item nav-link">Contact</a>
+        <router-link to="home-professionals" class="nav-item nav-link">Home</router-link>
+        <router-link to="/all-consults" class="nav-item nav-link">Consults</router-link>
+        <router-link to="/about" class="nav-item nav-link">About</router-link>
+        <router-link to="/contact" class="nav-item nav-link">Contact</router-link>
       </div>
       <a href="" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">Appointment<i class="fa fa-arrow-right ms-3"></i></a>
     </div>
