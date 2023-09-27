2bee9aacbb345467ee6fabb1d6c178bb1ee4415c
Kaan Uğur <kaan.ugur@hva.nl>
2023-09-27 14:18:48 +0200

    made changes footer

--- client/src/components/Footer/Footer.vue
+++ client/src/components/Footer/Footer.vue
@@ -17,28 +17,25 @@
         </div>
         <div class="col-lg-3 col-md-6">
           <h5 class="text-light mb-4">Services</h5>
-          <a class="btn btn-link" href="">Cardiology</a>
-          <a class="btn btn-link" href="">Pulmonary</a>
-          <a class="btn btn-link" href="">Neurology</a>
-          <a class="btn btn-link" href="">Orthopedics</a>
-          <a class="btn btn-link" href="">Laboratory</a>
+          <div class="services-footer">
+            <a>Cardiology</a>
+            <br>
+            <a>Pulmonary</a>
+            <br>
+            <a>Neurology</a>
+            <br>
+            <a>Orthopedics</a>
+            <br>
+            <a>Laboratory</a>
+          </div>
         </div>
         <div class="col-lg-3 col-md-6">
           <h5 class="text-light mb-4">Quick Links</h5>
-          <a class="btn btn-link" href="">About Us</a>
-          <a class="btn btn-link" href="">Contact Us</a>
-          <a class="btn btn-link" href="">Our Services</a>
-          <a class="btn btn-link" href="">Terms & Condition</a>
-          <a class="btn btn-link" href="">Support</a>
+          <router-link to="about" class="btn btn-link">About Us</router-link>
+          <router-link to="#" class="btn btn-link">Contact Us</router-link>
+          <router-link to="find-doctor" class="btn btn-link">Our Doctors</router-link>
         </div>
-        <div class="col-lg-3 col-md-6">
-          <h5 class="text-light mb-4">Newsletter</h5>
-          <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-          <div class="position-relative mx-auto" style="max-width: 400px;">
-            <input class="form-control border-0 w-100 py-3 ps-4 pe-5" type="text" placeholder="Your email">
-            <button type="button" class="btn btn-primary py-2 position-absolute top-0 end-0 mt-2 me-2">SignUp</button>
-          </div>
-        </div>
+
       </div>
     </div>
     <div class="container">
