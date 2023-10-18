35dbb2a7dc1e51442e7342509bb3732f7b64c246
raihanJa <raihanja@gmail.com>
2023-10-18 12:16:39 +0200

    fixed navbar

--- client/src/components/Navigation/Navbar.vue
+++ client/src/components/Navigation/Navbar.vue
@@ -1,55 +1,40 @@
 <template>
-  <!-- Topbar Start -->
-  <div class="container-fluid bg-light p-0 wow fadeIn" data-wow-delay="0.1s">
-    <div class="row gx-0 d-none d-lg-flex">
-      <div class="col-lg-7 px-5 text-start">
-        <div class="h-100 d-inline-flex align-items-center py-3 me-4">
-          <small class="fa fa-map-marker-alt text-primary me-2"></small>
-          <small>123 Street, Amsterdam, Netherlands</small>
-        </div>
-        <div class="h-100 d-inline-flex align-items-center py-3">
-          <small class="far fa-clock text-primary me-2"></small>
-          <small>Mon - Fri : 09.00 AM - 09.00 PM</small>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #5986CE;">
+    <div class="container">
+      <!-- Title (Mindease) -->
+      <!-- Logo -->
+      <div class="navbar-brand">
+        <img src="../../assets/img/logo.jpeg" alt="Mindease Logo" class="logo-img">
       </div>
-      <div class="col-lg-5 px-5 text-end">
-        <div class="h-100 d-inline-flex align-items-center py-3 me-4">
-          <small class="fa fa-phone-alt text-primary me-2"></small>
-          <small>+012 345 6789</small>
-        </div>
-        <div class="h-100 d-inline-flex align-items-center">
-          <a class="btn btn-sm-square rounded-circle bg-white text-primary me-1" href=""><i class="fab fa-facebook-f"></i></a>
-          <a class="btn btn-sm-square rounded-circle bg-white text-primary me-1" href=""><i class="fab fa-twitter"></i></a>
-          <a class="btn btn-sm-square rounded-circle bg-white text-primary me-1" href=""><i class="fab fa-linkedin-in"></i></a>
-          <a class="btn btn-sm-square rounded-circle bg-white text-primary me-0" href=""><i class="fab fa-instagram"></i></a>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- Topbar End -->
 
-  <!-- Navbar Start -->
-  <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
-    <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-      <h1 class="m-0 header-title"><i class="far fa-hospital me-3 icon "></i>Mindease</h1>
-    </a>
-    <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <div class="navbar-nav ms-auto p-4 p-lg-0">
-        <router-link to="home-professionals" class="nav-item nav-link">Home</router-link>
-        <router-link to="/all-consults" class="nav-item nav-link">Consults</router-link>
-        <router-link to="/about" class="nav-item nav-link">About</router-link>
-        <router-link to="/contact" class="nav-item nav-link">Contact</router-link>
+      <!-- Navbar Toggler for Mobile -->
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Navbar Menu Items and Login Button -->
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <!-- Menu Items -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+
+        <!-- Login Button -->
+        <button class="btn btn-primary">Login</button>
       </div>
-      <router-link to="/login" class="btn btn-primary rounded-0 py-4 px-lg-5 d-none d-lg-block">
-        Log in<i class="fa fa-arrow-right ms-3"></i>
-      </router-link>
     </div>
   </nav>
-  <!-- Navbar End -->
 </template>
+
 
 <script>
 export default {
@@ -58,198 +43,10 @@ export default {
 </script>
 
 <style scoped>
-@import 'bootstrap/dist/css/bootstrap.min.css';
 
-:root {
-  --primary: #05A3A4;
-  --light: #E8891D;
-  --dark: #006373;
+/* Custom style for the logo image */
+.logo-img {
+  max-width: 150px; /* Set the maximum width for the logo */
 }
 
-.header-title{
-  color: #006373;
-}
-
-.icon{
-  color: #006373;
-}
-
-/*** Navbar ***/
-.navbar .dropdown-toggle::after {
-  border: none;
-  content: "\f107";
-  font-family: "Font Awesome 5 Free";
-  font-weight: 900;
-  vertical-align: middle;
-  margin-left: 8px;
-}
-
-.navbar .navbar-nav .nav-link {
-  margin-right: 30px;
-  padding: 25px 0;
-  color: #FFFFFF;
-  font-size: 15px;
-  font-weight: 500;
-  text-transform: uppercase;
-  outline: none;
-}
-
-.navbar .navbar-nav .nav-link:hover,
-.navbar .navbar-nav .nav-link.active {
-  color: var(--primary);
-}
-
-@media (max-width: 991.98px) {
-  .navbar .navbar-nav .nav-link {
-    margin-right: 0;
-    padding: 10px 0;
-  }
-
-  .navbar .navbar-nav {
-    border-top: 1px solid #EEEEEE;
-  }
-}
-
-.navbar .navbar-brand,
-.navbar a.btn {
-  height: 75px;
-}
-
-.navbar .navbar-nav .nav-link {
-  color: var(--dark);
-  font-weight: 500;
-}
-
-.navbar.sticky-top {
-  top: -100px;
-  transition: .5s;
-}
-
-@media (min-width: 992px) {
-  .navbar .nav-item .dropdown-menu {
-    display: block;
-    border: none;
-    margin-top: 0;
-    top: 150%;
-    opacity: 0;
-    visibility: hidden;
-    transition: .5s;
-  }
-
-  .navbar .nav-item:hover .dropdown-menu {
-    top: 100%;
-    visibility: visible;
-    transition: .5s;
-    opacity: 1;
-  }
-}
-
-:root {
-  --primary: #05A3A4;
-  --light: #E8891D;
-  --dark: #006373;
-}
-
-/*** Navbar ***/
-.navbar .dropdown-toggle::after {
-  border: none;
-  content: "\f107";
-  font-family: "Font Awesome 5 Free",serif;
-  font-weight: 900;
-  vertical-align: middle;
-  margin-left: 8px;
-}
-
-.navbar .navbar-nav .nav-link {
-  margin-right: 30px;
-  padding: 25px 0;
-  color: #FFFFFF;
-  font-size: 15px;
-  font-weight: 500;
-  text-transform: uppercase;
-  outline: none;
-}
-
-.navbar .navbar-nav .nav-link:hover,
-.navbar .navbar-nav .nav-link.active {
-  color: var(--primary);
-}
-
-@media (max-width: 991.98px) {
-  .navbar .navbar-nav .nav-link {
-    margin-right: 0;
-    padding: 10px 0;
-  }
-
-  .navbar .navbar-nav {
-    border-top: 1px solid #EEEEEE;
-  }
-}
-
-.navbar .navbar-brand,
-.navbar a.btn {
-  height: 75px;
-}
-
-.navbar .navbar-nav .nav-link {
-  color: var(--dark);
-  font-weight: 500;
-}
-
-.navbar.sticky-top {
-  top: -100px;
-  transition: .5s;
-}
-
-@media (min-width: 992px) {
-  .navbar .nav-item .dropdown-menu {
-    display: block;
-    border: none;
-    margin-top: 0;
-    top: 150%;
-    opacity: 0;
-    visibility: hidden;
-    transition: .5s;
-  }
-
-  .navbar .nav-item:hover .dropdown-menu {
-    top: 100%;
-    visibility: visible;
-    transition: .5s;
-    opacity: 1;
-  }
-}
-
-.btn.btn-primary {
-  background-color: #05A3A4;
-  border-color: #05A3A4;
-}
-
-.fa-instagram:before {
-  color: #006373;
-}
-
-.fa-linkedin-in:before {
-  color: #006373;
-}
-
-.fa-twitter:before {
-  color: #006373;
-}
-
-.fa-facebook-f:before {
-  color: #006373;
-}
-
-.fa-phone-alt:before {
-  color: #006373;
-}
-
-.fa-clock:before {
-  color: #006373;
-}
-
-.fa-map-marker-alt:before {
-  color: #006373;
-}
 </style>
