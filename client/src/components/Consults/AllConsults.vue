d981fac2d2155a30722981e02054a5979d57fc85
raihanJa <raihanja@gmail.com>
2023-10-25 13:45:04 +0200

    consultant patient page

--- client/src/components/Consults/AllConsults.vue
+++ client/src/components/Consults/AllConsults.vue
@@ -1,20 +1,78 @@
 <template>
   <!-- Service Start -->
-  <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
-    <p class="d-inline-block border rounded-pill py-1 px-4">All Consults</p>
+  <div class="text-center mx-auto mb-1 mt-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+    <p class="d-inline-block border rounded-pill py-1 px-4">Consults</p>
   </div>
 
-  <consults-list :consults="dummyConsults"></consults-list>
-  <!-- Service End -->
+  <div class="container">
+
+    <!-- First Row -->
+    <div class="row border-bottom">
+      <div class="col">
+        <h3 class="title">Attending consultancies</h3>
+      </div>
+    </div>
+
+    <!-- Second Row -->
+    <div class="row">
+      <!-- First Column -->
+      <div class="col-md-4">
+        <ConsultantComponent name="Dr. Bibber" date="September 30, 2023" time="2:00 PM - 3:00 PM" consult="Heart-related issues" past="past" current="current"/>
+      </div>
+    </div>
+
+    <!-- First Row -->
+    <div class="row border-bottom">
+      <div class="col">
+        <h3 class="title">Upcoming consultancies</h3>
+      </div>
+    </div>
+
+    <!-- Second Row -->
+    <div class="row">
+      <!-- First Column -->
+      <div class="col-md-4">
+        <ConsultantComponent name="Dr. Smith" date="September 28, 2023" time="2:00 PM - 3:00 PM" consult="Heart-related issues" />
+      </div>
+      <!-- Second Column -->
+      <div class="col-md-4">
+        <ConsultantComponent name="Dr. Johnson" date="October 5, 2023" time="10:30 AM - 11:30 AM" consult="Mental Health" />
+      </div>
+    </div>
+
+    <!-- First Row -->
+    <div class="row border-bottom">
+      <div class="col">
+        <h3 class="title">Past Consultancies</h3>
+      </div>
+    </div>
+
+    <!-- Second Row -->
+    <div class="row">
+      <!-- First Column -->
+      <div class="col-md-4">
+        <ConsultantComponent name="Dr. Patel" date="October 12, 2023" time="3:30 PM - 4:30 PM" consult="Diabetes Management" past="past"/>
+      </div>
+      <!-- Second Column -->
+      <div class="col-md-4">
+        <ConsultantComponent name="Dr. Lee" date="October 18, 2023" time="11:00 AM - 12:00 PM" consult="Physical Therapy"  past="past"/>
+      </div>
+      <!-- Third Column -->
+      <div class="col-md-4">
+        <ConsultantComponent name="Dr. Garcia" date="October 25, 2023" time="1:30 PM - 2:30 PM" consult="Nutrition Counseling"  past="past"/>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import ConsultsList from "@/components/Consults/ConsultsList";
+import ConsultantComponent from "@/components/Consults/ConsultComponent";
 
 export default {
   name: "AllClient",
   components: {
-    ConsultsList
+    ConsultantComponent
   },
   data() {
     return {
---
+++ client/src/components/Consults/ConsultComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container-xxl py-5">
+    <div class="container">
+      <div class="row g-4">
+        <div class="service-item bg-light rounded h-100 p-5">
+          <div class="d-inline-flex align-items-center justify-content-center bg-white rounded-circle mb-4"
+               style="width: 65px; height: 65px;">
+            <i class="fa fa-heartbeat text-primary fs-4"></i>
+          </div>
+          <h4 class="mb-3">{{ name }}</h4>
+          <p class="mb-2"><strong>Date:</strong> {{ date }}</p>
+          <p class="mb-2"><strong>Time:</strong> {{ time }}</p>
+          <p class="mb-4"><strong>Consulting for:</strong> {{ consult }}</p>
+          <button v-if="!past" class="custom-button">Join Session</button>
+          <button v-if="current" class="custom-button-two">Cancel Session</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ConsultantComponent",
+  props: {
+    name: String,
+    date: String,
+    time: String,
+    consult: String,
+    past: Boolean,
+    current: Boolean
+  }
+
+}
+</script>
+
+<style scoped>
+.fa-heartbeat:before {
+  color: #E8891D;
+}
+
+.custom-button{
+  color: black;
+  background-color: lawngreen;
+  padding: 3%;
+  border-radius: 20px;
+  transition: 0.5s;
+}
+
+.custom-button:hover{
+  background-color: #5986CE;
+  color: white;
+}
+
+.custom-button-two{
+  color: red;
+  background-color: transparent;
+  border: 2px solid red;
+  padding: 3%;
+  border-radius: 20px;
+  transition: 0.5s;
+}
+
+.custom-button-two:hover{
+  color: white;
+  background-color: red;
+}
+</style>
