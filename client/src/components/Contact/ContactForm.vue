e3db447f84fd04d4adc2e9beb52b005186ead826
raihanJa <raihanja@gmail.com>
2023-10-30 14:07:12 +0100

    merge fix

--- client/src/components/Contact/ContactForm.vue
+++ client/src/components/Contact/ContactForm.vue
@@ -1,43 +1,42 @@
 <template>
-
   <div class="col wow fadeIn" data-wow-delay="0.1s">
-    <div class="bg-light rounded p-5">
-      <p class="d-inline-block border rounded-pill py-1 px-4">Contact Me</p>
-      <h1 class="mb-4">Send me a message</h1>
-      <form>
-        <div class="row g-3">
-          <div class="col-md-6">
-            <div class="form-floating">
-              <input type="text" class="form-control" id="name" placeholder="Your Name">
-              <label for="name">Your Name</label>
-              <div class="col-md-6">
-                <div class="form-floating">
-                  <input type="email" class="form-control" id="email" placeholder="Your Email">
-                  <label for="email">Your Email</label>
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="form-floating">
-                  <input type="text" class="form-control" id="subject" placeholder="Subject">
-                  <label for="subject">Subject</label>
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="form-floating">
-                    <textarea class="form-control" placeholder="Leave a message here" id="message"
-                              style="height: 100px"></textarea>
-                  <label for="message">Message</label>
-                </div>
-              </div>
-              <div class="col-12">
-                <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+      <div class="bg-light rounded p-5">
+        <p class="d-inline-block border rounded-pill py-1 px-4">Contact Me</p>
+        <h1 class="mb-4">Contact Form</h1>
+        <form>
+          <div class="row g-3">
+            <div class="col-md-6">
+              <div class="form-floating">
+                <input type="text" class="form-control" id="name" placeholder="Your Name">
+                <label for="name">Your Name</label>
               </div>
             </div>
+            <div class="col-md-6">
+              <div class="form-floating">
+                <input type="email" class="form-control" id="email" placeholder="Your Email">
+                <label for="email">Your Email</label>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-floating">
+                <input type="text" class="form-control" id="subject" placeholder="Subject">
+                <label for="subject">Subject</label>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-floating">
+                    <textarea class="form-control" placeholder="Leave a message here" id="message"
+                              style="height: 100px"></textarea>
+                <label for="message">Message</label>
+              </div>
+            </div>
+            <div class="col-12">
+              <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+            </div>
           </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -61,7 +60,7 @@ export default {
   border-color: #05A3A4;
 }
 
-.rounded-pill {
+.rounded-pill{
   color: #006373;
 }
 
