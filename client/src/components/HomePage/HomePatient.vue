069d5443308cb6d4b4a8f9749794b5581f1b3260
PixLNickz <63245924+PixLNickz@users.noreply.github.com>
2023-11-22 15:20:12 +0100

    onderste tekst ook naar links

--- client/src/components/HomePage/HomePatient.vue
+++ client/src/components/HomePage/HomePatient.vue
@@ -163,7 +163,7 @@
       <div class="row g-5">
         <div class="col-lg-6 d-flex flex-column align-items-center justify-content-center">
           <h2 class="text-center mb-xxl-5">Why Sign Up?</h2>
-          <p class="text-center">
+          <p class="text-left">
             ✨ <span style="color: #5986CE; font-weight: bold">Personalized Care:</span> Our experts understand that
             every individual is unique. We craft personalized health plans tailored to your specific needs.<br>
             ✨ <span style="color: #5986CE; font-weight: bold">Expert Guidance: </span>Access renowned healthcare
