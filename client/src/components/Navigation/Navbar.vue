b8f70109a1c35881c57cccdbe028fd4c029b356d
PixLNickz <63245924+PixLNickz@users.noreply.github.com>
2023-11-29 13:18:41 +0100

    navbar fix

--- client/src/components/Navigation/Navbar.vue
+++ client/src/components/Navigation/Navbar.vue
@@ -16,11 +16,11 @@
       <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <!-- Menu Items -->
         <ul class="navbar-nav">
-          <li class="nav-item" v-if="['patient-consults', 'logged-patient', 'findDoctor', 'appointment'].includes($route.name)">
+          <li class="nav-item" v-if="['patient-consults', 'home-patient', 'logged-patient', 'findDoctor', 'appointment'].includes($route.name)">
             <router-link to="/patient-consults" class="nav-link">Your Consults</router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/about" class="nav-link">About</router-link>
+            <router-link to="/about" class="nav-link">Home</router-link>
           </li>
           <li class="nav-item">
             <router-link to="/contact" class="nav-link">Contact</router-link>
