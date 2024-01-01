f9dda0da4ffc8032461d5dc10ec652829db97603
raihanJa <raihanja@gmail.com>
2024-01-01 15:11:54 +0100

    Changed contact

--- client/src/components/Contact/Contact.vue
+++ client/src/components/Contact/Contact.vue
@@ -3,9 +3,7 @@
   <div class="container-xxl py-5">
     <div class="container">
       <div class="row g-4">
-        <Adress></Adress>
         <MailUs></MailUs>
-        <CallUs></CallUs>
 
         <ContactForm></ContactForm>
       </div>
@@ -15,14 +13,12 @@
 </template>
 
 <script>
-import Adress from "@/components/Contact/Adress";
-import CallUs from "@/components/Contact/CallUs";
 import MailUs from "@/components/Contact/MailUs";
 import ContactForm from "@/components/Contact/ContactForm";
 
 export default {
   name: "ContactComponent",
-  components: { Adress, CallUs, MailUs, ContactForm,}
+  components: {  MailUs, ContactForm,}
 }
 </script>
 
