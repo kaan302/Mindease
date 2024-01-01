e3a99bd5e6b577cdcd6ab1154c24daf6680e5e54
raihanJa <raihanja@gmail.com>
2024-01-01 16:18:16 +0100

    changed about content

--- client/src/components/About/About.vue
+++ client/src/components/About/About.vue
@@ -22,9 +22,15 @@
         </div>
         <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
           <p class="d-inline-block border rounded-pill py-1 px-4">About Us</p>
-          <h1 class="mb-4">Why You Should Trust Us? Get Know About Us!</h1>
-          <p>At Mindease, we understand the significance of mental well-being in today's fast-paced world. We are more than just a platform; we are a community of caring professionals and individuals dedicated to making mental healthcare accessible, approachable, and effective.</p>
-          <p class="mb-4">Our mission at Mindease is simple yet profound: to empower lives by providing exceptional mental health support. We believe that everyone deserves access to high-quality mental health services tailored to their unique needs. By bridging the gap between individuals and mental health professionals, we facilitate a safe and nurturing environment where healing begins.</p>
+          <h1 class="mb-4">Why You Should Trust Us? <br> Get To Know About Us!</h1>
+          <p>Welcome to MindEase, a sanctuary for mental health and well-being. We
+            understand that seeking support for your mental health is a deeply personal
+            journey, and we’re here to guide you through it with empathy, compassion, and
+            transparency</p><br>
+          <p>At MindEase, we believe in a holistic approach to mental health. Our team of
+            experienced professionals combines evidence-based practices with a genuine
+            understanding of individual needs. We recognize that every person’s path to
+            healing is unique, and we tailor our support to meet you where you are.</p>
         </div>
       </div>
     </div>
@@ -40,57 +46,51 @@
           <div class="p-lg-5 ps-lg-0">
             <p class="d-inline-block border rounded-pill text-light py-1 px-4">Features</p>
             <h1 class="text-white mb-4">Why Choose Us</h1>
-            <p class="text-white mb-4 pb-2">Compassionate Care: Our team of experienced therapists and counselors are deeply compassionate about mental health. We approach every individual with empathy and understanding, ensuring you feel heard and supported.
+            <p class="text-white mb-4 pb-2">
+              <b>Personalized Care:</b> We don’t believe in a one-size-fits-all approach. Our
+              dedicated team takes the time to listen, understand, and create a customized
+              plan that addresses your specific concerns and goals.<br><br>
+              <b>Community and Connection:</b> We know that feeling heard and understood is
+              crucial for healing. That’s why we foster a supportive community where you can
+              share, learn, and grow alongside others who are on similar journeys.<br><br>
+              <b>Innovative Therapeutic Techniques:</b> We’re committed to staying at the
+              forefront of mental health care. Our team continuously explores and integrates
+              innovative techniques and therapies to provide you with the most effective
+              support.
 
-              Holistic Approach: We recognize that mental health is interconnected with various aspects of life. Our approach is holistic, considering not only your symptoms but also the underlying causes and personal circumstances.</p>
+            </p>
             <div class="row g-4">
-              <div class="col-6">
+              <div class="col">
                 <div class="d-flex align-items-center">
                   <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style="width: 55px; height: 55px;">
                     <i class="fa fa-user-md text-primary"></i>
                   </div>
                   <div class="ms-4">
-                    <p class="text-white mb-2">Experience</p>
-                    <h5 class="text-white mb-0">Doctors</h5>
+                    <p class="text-white mb-2">Mission</p>
+                    <h5 class="text-white mb-0">Facilitate global access to mental health support by fostering connections
+                      between therapists and patients worldwide.</h5>
                   </div>
                 </div>
               </div>
-              <div class="col-6">
+            </div>
+
+            <div class="row g-4 mt-2">
+              <div class="col">
                 <div class="d-flex align-items-center">
                   <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style="width: 55px; height: 55px;">
                     <i class="fa fa-check text-primary"></i>
                   </div>
                   <div class="ms-4">
-                    <p class="text-white mb-2">Quality</p>
-                    <h5 class="text-white mb-0">Services</h5>
-                  </div>
-                </div>
-              </div>
-              <div class="col-6">
-                <div class="d-flex align-items-center">
-                  <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style="width: 55px; height: 55px;">
-                    <i class="fa fa-comment-medical text-primary"></i>
-                  </div>
-                  <div class="ms-4">
-                    <p class="text-white mb-2">Positive</p>
-                    <h5 class="text-white mb-0">Consultation</h5>
-                  </div>
-                </div>
-              </div>
-              <div class="col-6">
-                <div class="d-flex align-items-center">
-                  <div class="d-flex flex-shrink-0 align-items-center justify-content-center rounded-circle bg-light" style="width: 55px; height: 55px;">
-                    <i class="fa fa-headphones text-primary"></i>
-                  </div>
-                  <div class="ms-4">
-                    <p class="text-white mb-2">24 Hours</p>
-                    <h5 class="text-white mb-0">Support</h5>
+                    <p class="text-white mb-2">Vision</p>
+                    <h5 class="text-white mb-0">Build a globally accessible 24/7 mental health network, connecting individuals
+                      with therapists globally for immediate support and well-being.</h5>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
         <div class="col-lg-6 pe-lg-0 wow fadeIn" data-wow-delay="0.5s" style="min-height: 400px;">
           <div class="position-relative h-100">
             <img class="position-absolute img-fluid w-100 h-100" src="../../assets/img/feature.jpg" style="object-fit: cover;" alt="">
